8be3ef4cd0b251f6f88dc02aaff902591bd85392
Dumitru-radu <101594913+radu9120@users.noreply.github.com>
2023-12-23 20:39:54 +0000

    first commit

---
+++ components/ui/Brand/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import logoDark from "public/logo-dark.svg";
+
+export default () => (
+  <>
+    <div className=" h-32 w-32 absolute -top-6">
+      <Image src={logoDark} alt="Float UI logo" />
+    </div>
+  </>
+);
---
+++ metatag.ts
@@ -0,0 +1,5 @@
+export default {
+  title: "Covo Components - Beautiful Tailwind UI components & Templates",
+  desc: "Beautiful and responsive website templates and UI components for React, Vue, Svelte, HTML with Tailwind CSS.",
+  ogImage: "/thumbnail.png?v2",
+};
